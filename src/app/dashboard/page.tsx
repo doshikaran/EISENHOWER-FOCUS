26cfef55f8e88904e8f30749065edcef5918f307
doshikaran <doshikaran005@gmail.com>
2024-06-18 14:33:37 -0600

    authentication

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Dashboard = (props: Props) => {
+  return (
+    <div className = " h-screen bg-[#fefae0]">
+      dashboard
+    </div>
+  )
+}
+
+export default Dashboard
